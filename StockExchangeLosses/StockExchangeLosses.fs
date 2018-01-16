311cc39eb33b0b2c520e15ddea908413350928f6
Janus Schmidt <janusschmidt@gmail.com>
2018-01-16 16:26:56 +0100

    Added Stock exchange losses puzzle

---
+++ StockExchangeLosses/StockExchangeLosses.fs
@@ -0,0 +1,27 @@
+﻿module StockExchangeLosses.StockExchangeLosses
+
+open System
+
+type bestAnswer = {max:int; largestDrop:int}
+
+let losses quotes =
+    let updateMax a b = if a<b then b else a
+
+    let folder (agg:bestAnswer) (quote:int) =
+        let max = updateMax agg.max quote
+        let drop = max - quote
+        let largestdrop = updateMax agg.largestDrop drop
+        {max=max; largestDrop=largestdrop}
+
+    quotes |> List.fold folder {max=0;largestDrop=0}
+
+let run() =
+    let read() = Console.In.ReadLine()
+    let numberOFQuotes = read() |> int
+    let quotes = read().Split [|' '|] |> Array.map int |> Seq.toList
+
+    let {largestDrop=d} = losses quotes
+    if d>0 then
+        printfn "-%i" d
+    else
+        printfn "0"
---
+++ StockExchangeLosses/Tests/StockExchangeLossesTest.fs
@@ -0,0 +1,23 @@
+﻿module StockExchangeLosses.Tests.StockExchangeLossesTest
+
+open Xunit
+open StockExchangeLosses.StockExchangeLosses
+
+[<Fact>]
+let ``Example 1`` () =
+    let res = losses [3;2;4;2;1;5]
+    
+    Assert.Equal(3, res.largestDrop)
+
+[<Fact>]
+let ``Example 2`` () =
+    let res = losses [5;3;4;2;3;1]
+    
+    Assert.Equal(4, res.largestDrop)
+
+[<Fact>]
+let ``Example 3`` () =
+    let res = losses [1;2;4;4;5]
+    
+    Assert.Equal(0, res.largestDrop)
+
