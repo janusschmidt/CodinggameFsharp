16a19ec10f9c3377197d5d5ebbf47d9cfbe4ad8b
Janus Schmidt <janusschmidt@gmail.com>
2018-02-01 23:24:16 +0100

    Conway sequences done

---
+++ ConwaySequence/ConwaySequence.fs
@@ -0,0 +1,34 @@
+﻿module ConwaySequence.ConwaySequence
+
+let calculate (numbers:string) indexOfLineToCalculate =
+    let rec calculateInner (numberSeq:string list) linesLeftToCalculate =
+        let parseLine (lnumbers:string List) =
+            let l = "X"::lnumbers |> List.rev
+ 
+            let (_,_,newSeq) = l |> List.fold (fun (currentNumber, count, newSeq) item ->
+                match currentNumber with 
+                | None -> (Some item, 1, [])
+                | Some cur when cur=item -> (Some item, count + 1, newSeq)
+                | Some cur -> 
+                    let countAsString = count |> string
+                    (Some item, 1, countAsString::cur::newSeq)) (None, 0, [])
+                
+            newSeq
+ 
+        if linesLeftToCalculate < 2 then
+            numberSeq
+        else
+            let newSeq = numberSeq |> parseLine 
+            calculateInner newSeq (linesLeftToCalculate - 1)
+    
+    let res = calculateInner (numbers.Split [|' '|] |> Array.toList) indexOfLineToCalculate
+    res
+    
+let run _ =
+    let numberSeq = stdin.ReadLine()
+    let indexOfLineToCalculate = stdin.ReadLine() |> int
+    
+    let res = calculate numberSeq indexOfLineToCalculate
+    printfn "%s" (res |> List.reduce (fun i1 i2 -> i1 + " " + i2))
+
+//run ()
---
+++ ConwaySequence/Tests/ConwaySequenceTest.fs
@@ -0,0 +1,28 @@
+﻿module ConwaySequence.Tests.ConwaySequenceTest
+
+open Xunit
+
+[<Fact>]
+let ``ConwaySequence 1 6`` () =
+    let expected = 
+        ["1";
+        "11";
+        "21";
+        "1211";
+        "111221";
+        "312211"]
+
+    let res = ConwaySequence.ConwaySequence.calculate "1" 6
+    
+    Assert.Equal(expected.[6-1], res)
+
+
+[<Fact>]
+let ``ConwaySequence 25 10`` () =
+   
+    let res = ConwaySequence.ConwaySequence.calculate "25" 10
+    
+    Assert.Equal("311311222113111231133221125", res)
+
+
+
