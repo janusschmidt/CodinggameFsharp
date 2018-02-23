7c307f92b973c0017d13432c033a88acd77c4b00
Janus Schmidt <janus.schmidt@gmail.com>
2018-02-23 01:55:59 +0100

    Implemented PhoneNumbers. (Tries. Prefix tree)

---
+++ PhoneNumbers/PhoneNumbers.fs
@@ -0,0 +1,30 @@
+﻿module PhoneNumbers
+
+type prefixTreeNode = {value:char; nodes:prefixTreeNode list; isContentNode:bool}
+
+let countNodes node = 
+    let rec inner node = 
+        node.nodes |> List.fold (fun agg n -> agg + inner n) 1
+    (inner node) - 1
+
+let buildTrieTree (numbers:string list)=
+    let rec buildInner numbers=
+        let skipFirstChar s = Seq.skip 1 s |> Seq.toArray |> System.String
+        let isEmptyString (s:string) = s = ""
+        let isNotEmptyString = isEmptyString >> not
+        let prefixGroups = numbers |> List.filter isNotEmptyString |> List.groupBy (fun n -> n.[0])
+        prefixGroups |> List.map (fun g-> 
+            let key = fst g
+            let subNumbers = snd g |> List.map skipFirstChar
+            let isContentNode = subNumbers |> List.exists isEmptyString
+            {value=key;nodes=buildInner subNumbers; isContentNode=isContentNode}) 
+
+    {value=' ';nodes=buildInner numbers;isContentNode=false}
+
+let run () =
+    let N = stdin.ReadLine() |> int
+    let numbers = List.init N (fun n -> stdin.ReadLine())
+    let tree = buildTrieTree numbers
+    printfn "%i" (countNodes tree)
+
+//run ()
---
+++ PhoneNumbers/Tests/PhoneNumbersTest.fs
@@ -0,0 +1,32 @@
+﻿module PhoneNumbersTest
+
+open Xunit
+
+[<Fact>]
+let ``One number`` () =
+    let numbers = ["0467123456"]
+    let expectedNumberOfNodes = 10
+    let res = PhoneNumbers.buildTrieTree numbers  |> PhoneNumbers.countNodes
+    Assert.Equal(expectedNumberOfNodes, res)
+
+
+[<Fact>]
+let ``Two numbers`` () =
+    let numbers = ["0123456789";"1123456789"]
+    let expectedNumberOfNodes = 20
+    let res = PhoneNumbers.buildTrieTree numbers  |> PhoneNumbers.countNodes
+    Assert.Equal(expectedNumberOfNodes, res)
+
+[<Fact>]
+let ``Two numbers, different length`` () =
+    let numbers = ["0123456789";"0123"]
+    let expectedNumberOfNodes = 10
+    let res = PhoneNumbers.buildTrieTree numbers  |> PhoneNumbers.countNodes
+    Assert.Equal(expectedNumberOfNodes, res)
+
+[<Fact>]
+let ``Multiple numbers`` () =
+    let numbers = ["0412578440";"0412199803";"0468892011";"112";"15"]
+    let expectedNumberOfNodes = 28
+    let res = PhoneNumbers.buildTrieTree numbers  |> PhoneNumbers.countNodes
+    Assert.Equal(expectedNumberOfNodes, res)
