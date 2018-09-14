7fc12abd811ce7736c8929040a3a55ed3703957a
Janus Schmidt <janusschmidt@gmail.com>
2018-09-14 23:29:19 +0200

    Traffic lights initial solution

---
+++ TrafficLights/Tests/TrafficLightsTest.fs
@@ -0,0 +1,60 @@
+﻿module TrafficLights.Tests.TrafficLightsTest
+
+open Xunit
+open Microsoft.FSharp.Data.UnitSystems.SI.UnitSymbols
+open TrafficLights.TrafficLights
+open System.Diagnostics
+
+[<Fact>]
+let ``One light. Max speed will do it`` () =
+    let speed = TrafficLights.TrafficLights.getConsistentCruiseSpeed [{distance=200m<m>; duration=15m<s>}] (50m*kmh)
+    Assert.Equal(50, speed)
+
+
+[<Fact>]
+let ``One light. Max speed will not suffice`` () =
+    let speed = TrafficLights.TrafficLights.getConsistentCruiseSpeed [{distance=200m<m>; duration=10m<s>}] (50m*kmh)
+    Assert.Equal(36, speed)
+
+[<Fact>]
+let ``Three lights`` () =
+    let speed = 
+        TrafficLights.TrafficLights.getConsistentCruiseSpeed [
+            {distance=300m<m>; duration=30m<s>};
+            {distance=1500m<m>; duration=20m<s>};
+            {distance=3000m<m>; duration=10m<s>}] (90m*kmh)
+    Assert.Equal(67, speed)
+
+[<Fact>]
+let ``German highway`` () =
+    let speed = 
+        TrafficLights.TrafficLights.getConsistentCruiseSpeed [
+            {distance=1000m<m>; duration=15m<s>};
+            {distance=3000m<m>; duration=10m<s>};
+            {distance=4000m<m>; duration=30m<s>};
+            {distance=5000m<m>; duration=30m<s>};
+            {distance=6000m<m>; duration=5m<s>};
+            {distance=7000m<m>; duration=10m<s>};] (200m*kmh)
+    Assert.Equal(60, speed)
+
+[<Fact>]
+let ``Fast lights`` () =
+    let speed = 
+        TrafficLights.TrafficLights.getConsistentCruiseSpeed [
+            {distance=1234m<m>;  duration=5m<s>};
+            {distance=2468m<m>;  duration=5m<s>};
+            {distance=3702m<m>;  duration=5m<s>};
+            {distance=6170m<m>;  duration=5m<s>};
+            {distance=8638m<m>;  duration=5m<s>};
+            {distance=13574m<m>; duration=5m<s>};
+            {distance=16042m<m>; duration=5m<s>};
+            {distance=20978m<m>; duration=5m<s>};
+            {distance=23446m<m>; duration=5m<s>};
+            {distance=28382m<m>; duration=5m<s>};
+            {distance=35786m<m>; duration=5m<s>};
+            {distance=38254m<m>; duration=5m<s>};
+            {distance=45658m<m>; duration=5m<s>};
+            {distance=50594m<m>; duration=5m<s>};
+            {distance=53062m<m>; duration=5m<s>};
+            {distance=57998m<m>; duration=5m<s>};] (90m*kmh)
+    Assert.Equal(74, speed)
---
+++ TrafficLights/TrafficLights.fs
@@ -0,0 +1,79 @@
+﻿module TrafficLights.TrafficLights
+
+open System
+open Microsoft.FSharp.Data.UnitSystems.SI.UnitSymbols
+
+/// d = distance to light
+/// t = time between shifts
+/// conditions for hitting the green light in turn n:
+/// speed >= d/(((n*2)+1)*t)
+/// speed < d/(n*2*t)(except for n=0)
+/// 
+/// n = 0
+/// speed >= d/t
+/// speed < infinity
+/// 
+/// n = 1
+/// speed >= d/3t 
+/// speed < d/2t
+/// 
+///
+/// and so forth
+
+type trafficLightInfo = {distance:decimal<m>; duration:decimal<s>}
+
+let kmh = 1000m<m>/3600m<s>
+
+let getConsistentCruiseSpeed lights speedlimit =
+    let r10ms d = Decimal.Round(d|>decimal,10)*1m<m/s>
+
+    let kmhFloor ms = 
+        let kmhspeed = Decimal.Round(ms / kmh, 10)
+        let kmhspeedFloor = floor kmhspeed
+        let msSpeed = kmhspeedFloor * kmh
+        r10ms msSpeed
+
+    let rec getCruiseSpeed lights speedlimit =
+        let getCruiseSpeedForOneLight light speedlimit =
+            let minspeed turn = light.distance / ((1m + decimal(turn) * 2m) * light.duration) |> r10ms
+            let maxspeed turn = (if turn=0 then speedlimit else light.distance / (decimal(turn) * 2m * light.duration)) |> kmhFloor
+            let maxTurnSpeed = 
+                Seq.initInfinite (fun i -> (minspeed i, maxspeed i)) 
+                |> Seq.find (fun (min, _) -> min < speedlimit)
+                |> snd
+            min maxTurnSpeed speedlimit
+
+        match lights with
+        | h :: t -> getCruiseSpeed t (getCruiseSpeedForOneLight h speedlimit)
+        | [] -> speedlimit
+    
+    let speedlimitConvergentSeries = Seq.unfold (fun currentBestSpeedlimit -> 
+        if currentBestSpeedlimit = 0m<m/s> then 
+            None
+        else
+            let newSpeedLimit = getCruiseSpeed lights currentBestSpeedlimit
+            Some(newSpeedLimit, if newSpeedLimit = currentBestSpeedlimit then 0m<m/s> else newSpeedLimit)) (kmhFloor speedlimit)
+            
+    Seq.last speedlimitConvergentSeries / kmh |> round |> int
+
+let run () = 
+    let readIntMsg msg = 
+        let s = stdin.ReadLine()
+        eprintfn "%s: %s" msg s
+        s |> int
+
+    let speedLimit = (readIntMsg "Speedlimit" |> decimal) * kmh
+    let lightCount = readIntMsg "lightCount"
+    
+    let readLightsinfo _ = 
+        match (stdin.ReadLine()).Split [|' '|] |> Array.toList |> List.map int with
+        | h :: t :: [] -> 
+            eprintfn "distance:%i, duration:%i" h t
+            {distance=decimal(h)*1m<m>; duration=decimal(t)*1m<s>}
+        | _ -> raise (System.ArgumentException("traffic light info incomplete"))
+
+    let lights = List.init lightCount readLightsinfo
+    let cruisespeed = getConsistentCruiseSpeed lights speedLimit
+    printfn "%i" cruisespeed
+
+//run ()
